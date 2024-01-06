c39a6180439f7381b26faedb8ab5397438d1fa99
piotrzalejski <piotrzalejski@outlook.com>
2024-01-05 21:03:25 -0600

    minor changes

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -10,7 +10,6 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '../lib/actions';
-import { userAgentFromString } from 'next/server';
 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
